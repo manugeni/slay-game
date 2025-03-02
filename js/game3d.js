0461c1d89f9b250d3cbde5e671db94b702b0d446
Rendani <rendaniman@outlook.com>
2025-03-02 23:10:49 +0200

    Update game3d.js

--- js/game3d.js
+++ js/game3d.js
@@ -106,3 +106,44 @@ constructor() {
     this.animate = this.animate.bind(this);
     this.animate();
 }
+
+// In game3d.js, add the following method
+setupSkybox() {
+    const path = 'https://threejs.org/examples/textures/cube/SwedishRoyalCastle/';
+    const format = '.jpg';
+    const urls = [
+        path + 'px' + format, path + 'nx' + format,
+        path + 'py' + format, path + 'ny' + format,
+        path + 'pz' + format, path + 'nz' + format
+    ];
+
+    const reflectionCube = new THREE.CubeTextureLoader().load(urls);
+    reflectionCube.format = THREE.RGBFormat;
+
+    this.scene.background = reflectionCube;
+}
+
+// Call setupSkybox in the constructor
+constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('game-container').appendChild(this.renderer.domElement);
+
+    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+
+    this.player = new Player3D(this.scene);
+    this.enemies = [];
+    this.weapons = new Weapons3D(this.scene);
+
+    this.camera.position.z = 10;
+
+    this.setupLighting();
+    this.setupGround();
+    this.setupSkybox();
+    this.setupEventListeners();
+
+    this.animate = this.animate.bind(this);
+    this.animate();
+}
